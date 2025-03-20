3945ff061335b0d1228788123fe7de630450b78b
DriesBos <info@driesbos.com>
2025-03-20 23:09:07 +0900

    Subscribe form interaction

--- src/app/components/subscribe.tsx
+++ src/app/components/subscribe.tsx
@@ -3,24 +3,37 @@ import { useState } from 'react';
 
 export default function Subscribe() {
   const [email, setEmail] = useState('');
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
 
   const sendEmail = () => {
     console.log(`Email sent to: ${email}`);
     setEmail('');
+    setIsSuccess(true);
   };
+
   return (
     <div className="subscribe">
-      <label htmlFor="email">subscribe to stay in the loop</label>
-      <input
-        type="email"
-        id="email"
-        placeholder="your email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <button onClick={sendEmail}>
-        <img src="/return.svg" alt="submit" />
-      </button>
+      {!isSuccess && (
+        <>
+          <label htmlFor="email">subscribe to stay in the loop</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button onClick={sendEmail}>
+            <img src="/return.svg" alt="submit" />
+          </button>
+        </>
+      )}
+      {isSuccess && (
+        <>
+          <label>&nbsp;</label>
+          <p>Success. Thank you!</p>
+        </>
+      )}
     </div>
   );
 }

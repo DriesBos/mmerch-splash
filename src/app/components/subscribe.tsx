01d48cd033eb0c2cdb5854948483ad18e77c6986
DriesBos <info@driesbos.com>
2025-03-26 15:58:19 +0900

    update: subscribe component API call

--- src/app/components/subscribe.tsx
+++ src/app/components/subscribe.tsx
@@ -1,24 +1,58 @@
 'use client';
-import { useEffect, useState } from 'react';
+
+import { useEffect, useState, useRef } from 'react';
 
 export default function Subscribe() {
   const [email, setEmail] = useState('');
   const [isSuccess, setIsSuccess] = useState<boolean>(false);
-  const [isSendReady, setIsSendReady] = useState<boolean>(false);
+  const [buttonActive, setButtonActive] = useState<boolean>(false);
 
-  const sendEmail = () => {
-    console.log(`Email sent to: ${email}`);
-    setEmail('');
-    setIsSuccess(true);
-    setIsSendReady(false);
+  const sendEmail = async () => {
+    try {
+      const response = await fetch(
+        'https://mmerch.api-us1.com/admin/api.php?api_action=contact_sync',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Api-Token':
+              '43df38d9e7423fc060ee9d4ebf88769f649c43e7b9d74a1b9824f9060d218508802bda64',
+            api_action: 'contact_sync',
+            api_output: 'serialize',
+          },
+          body: JSON.stringify({
+            email: email,
+            field: [345, 0],
+            account_name: 'mmerch.activehosted.com',
+          }),
+        }
+      );
+
+      if (response.ok) {
+        console.log(`Email sent to: ${email}`);
+        setEmail('');
+        setIsSuccess(true);
+        setButtonActive(false);
+      } else {
+        console.error('Failed to send email');
+        setEmail('');
+        setIsSuccess(false);
+        setButtonActive(true);
+      }
+    } catch (error) {
+      setEmail('');
+      setIsSuccess(false);
+      setButtonActive(true);
+      console.error('Error sending email:', error);
+    }
   };
 
   useEffect(() => {
     if (email.length > 0) {
-      setIsSendReady(true);
+      setButtonActive(true);
       console.log(`Email is ready: ${email}`);
     } else {
-      setIsSendReady(false);
+      setButtonActive(false);
       console.log(`Email is not ready: ${email}`);
     }
   }, [email]);
@@ -36,7 +70,7 @@ export default function Subscribe() {
       )}
       {isSuccess && <p>Success. Thank you!</p>}
       <button
-        className={`${isSendReady === true ? 'active' : null}`}
+        className={`${buttonActive === true ? 'active' : null}`}
         onClick={sendEmail}
       >
         <img src="/return.svg" alt="submit" />

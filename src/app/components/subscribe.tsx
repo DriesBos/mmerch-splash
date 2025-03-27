13fbc68fa588eac1ec33e38f42436ae35042fa60
DriesBos <info@driesbos.com>
2025-03-28 00:37:44 +0900

    added two api calls for subscribe

--- src/app/components/subscribe.tsx
+++ src/app/components/subscribe.tsx
@@ -6,71 +6,80 @@ export default function Subscribe() {
   const [inputValue, setInputValue] = useState('');
   const [isSuccess, setIsSuccess] = useState<boolean>(false);
   const [isError, setIsError] = useState(false);
-  const inputRef = useRef<HTMLInputElement>(null);
+  // const inputRef = useRef<HTMLInputElement>(null);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
     setIsError(false);
     setIsSuccess(false);
-    inputRef.current?.setCustomValidity(''); // clear any custom validation messages
+    // inputRef.current?.setCustomValidity(''); // clear any custom validation messages
+  };
+
+  const createContact = async () => {
+    const options = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+        'Api-Token':
+          '43df38d9e7423fc060ee9d4ebf88769f649c43e7b9d74a1b9824f9060d218508802bda64',
+      },
+      body: JSON.stringify({
+        contact: {
+          inputValue,
+        },
+      }),
+    };
+
+    try {
+      const response = await fetch(
+        'https://mmerch.api-us1.com/api/3/contacts',
+        options
+      );
+      const result = await response.json();
+      console.log(result);
+      return result;
+    } catch (error) {
+      console.error('Error creating contact:', error);
+      throw error;
+    }
   };
 
   const handleSubmit = async () => {
+    const options = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+        'Api-Token':
+          '43df38d9e7423fc060ee9d4ebf88769f649c43e7b9d74a1b9824f9060d218508802bda64',
+      },
+      body: JSON.stringify({
+        contactList: { sourceid: 0, list: 2, contact: 1, status: 1 },
+      }),
+    };
+
     try {
       const response = await fetch(
-        'https://mmerch.api-us1.com/admin/api.php?api_action=contact_sync',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Api-Token':
-              '43df38d9e7423fc060ee9d4ebf88769f649c43e7b9d74a1b9824f9060d218508802bda64',
-            api_action: 'contact_sync',
-            api_output: 'serialize',
-          },
-          body: JSON.stringify({
-            email: inputValue,
-            field: [345, 0],
-            account_name: 'mmerch.activehosted.com',
-          }),
-        }
+        'https://mmerch.api-us1.com/api/3/contactLists',
+        options
       );
-
-      if (response.ok) {
-        console.log(`Email sent to: ${inputValue}`);
-        setInputValue('');
-        setIsSuccess(true);
-      } else {
-        setInputValue('');
-        setIsSuccess(false);
-        inputRef.current?.setCustomValidity(response.statusText);
-        inputRef.current?.reportValidity();
-        console.error('Error response:', response.statusText);
-      }
+      const result = await response.json();
+      console.log(result);
+      return result;
     } catch (error) {
-      setInputValue('');
-      setIsSuccess(false);
-      setIsError(true);
-      console.error('Error sending email:', error);
-      inputRef.current?.setCustomValidity(
-        'An error occurred. Please try again.'
-      );
-      inputRef.current?.reportValidity();
+      console.error('Error creating contactList item:', error);
+      throw error;
     }
   };
 
   return (
     <div className="subscribe">
-      <form
-        method="post"
-        target="_blank"
-        action="https://mmerch.activehosted.com/proc.php?jsonp=true"
-        onSubmit={handleSubmit}
-      >
+      <form method="post" onSubmit={() => handleSubmit()}>
         <input
           type="email"
           id="emailInput"
-          type={inputRef}
+          // ref={inputRef}
           name="email"
           min="4"
           pattern="[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}"
